dcc55c1ddf53ef9905e368ead8400b8d3a926e41
marcebenitez2 <marcebenitez0607@gmail.com>
2024-08-14 14:32:54 -0300

    feat: se agrego animacion a servicios

--- src/modules/Servicios/index.tsx
+++ src/modules/Servicios/index.tsx
@@ -1,15 +1,21 @@
+import { FramerComponent } from "@/shared/Framer/FramerComponent";
 import ServicesCard from "./Components/CardService/ServicesCard";
 import { DescriptionService } from "./Components/DescripcionService";
 
 export default function ServiciosModule() {
   return (
-    <div className="container mx-auto px-4 py-8">
+    <FramerComponent
+      style="container mx-auto px-4 py-28 "
+      animationInitial={{ y: 100, opacity: 0 }}
+      animationWhileInView={{ y: 0, opacity: 1 }}
+      animationViewPort={{ once: true, offset: 0.4 }}
+    >
       <h1 className="text-4xl font-bold mb-8 text-center text-sky-700">
         Ensayos no Destructivos
       </h1>
 
       <DescriptionService />
       <ServicesCard />
-    </div>
+    </FramerComponent>
   );
 }
